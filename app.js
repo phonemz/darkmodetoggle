2ee1b47f3a8c6577406cbef3cf44c11248bc18ad
Phonemz <phonemyintzaw3033@gmail.com>
2022-03-19 16:13:22 +0630

    completed

---
+++ app.js
@@ -0,0 +1,8 @@
+const toggle = document.querySelector('.toggle')
+const body = document.body
+const ball = document.querySelector('.ball')
+
+toggle.addEventListener('click', () => {
+    body.classList.toggle('dark')
+    ball.classList.toggle('move')
+})
